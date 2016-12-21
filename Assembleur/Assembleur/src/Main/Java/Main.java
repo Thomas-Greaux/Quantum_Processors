8ae066ec68b5667eb73b2a4c698ff17c51c9a6bd
Thomas Greaux <thomas.greaux@etu.unice.fr>
2016-12-21 17:37:10 +0100

    Made the project Maven-compatible

--- Assembleur/Assembleur/src/Main/Java/Main.java
+++ Assembleur/Assembleur/src/Main/Java/Main.java
@@ -3,7 +3,7 @@ import Traductor.Translator;
 public class Main {
 
 	public static void main(String[] args) {
-		String instruction = "ADD";
+		String instruction = "B";
 		Translator t = new Translator();
 		String res = t.toBin(instruction);
 		System.out.println(res);
